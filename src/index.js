a965dd659af029136459d2700de0cc71d3d69a2a
Chris <47066922+cwilso28@users.noreply.github.com>
2025-03-20 22:09:18 -0500

    Create task class
    The class will contain everything that is needed to define a task.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+class Task {
+    constructor(name, desc = '', project = 'Inbox', dueDate = 'Today') {
+        this.name = name;
+        this.desc = desc;
+        this.project = project;
+        this.dueDate = dueDate;
+    };
+
+    createDate = new Date();
+}
