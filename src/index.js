bbe926ab2e62ff87d4cfaea8ecd3f1ba05c9a802
Chris <47066922+cwilso28@users.noreply.github.com>
2025-03-22 23:08:07 -0500

    Create date formatter function
    The todo list only needs the date so the formatter drops the time
    portion of date and nicely formats the date for display at a later date.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 class Task {
-    constructor(name, desc = '', project = 'Inbox', dueDate = 'Today') {
+    constructor(name, desc = '', project = 'Inbox', dueDate = new Date()) {
         this.name = name;
         this.desc = desc;
         this.project = project;
         this.dueDate = dueDate;
     };
 
-    createDate = new Date();
+    createDate = dateFormatter(new Date()).formattedDate;
 }
+
+function dateFormatter(date) {
+    const options = {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric'
+    };
+
+    const formatter = new Intl.DateTimeFormat("en-US", options)
+
+    let formattedDate = formatter.format(date);
+
+    return { formattedDate }
+}
+
+newTask = new Task("Make bed","","",'Today');
+console.log(newTask.name);
+console.log(newTask.createDate)
+
