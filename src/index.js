9c0d9960d376501afce66934b62bb16302369a37
Chris <47066922+cwilso28@users.noreply.github.com>
2025-03-23 22:20:53 -0500

    Convert dates to exclude time and add check
    The dates don't need the time element and the added code removes the
    time element from the date time object. A check to see if the due date
    and created date are equal to add custom date formatting for future use.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 class Task {
-    constructor(name, desc = '', project = 'Inbox', dueDate = new Date()) {
+    constructor(name, desc = '', project = 'Inbox', dueDate = new Date(new Date().toDateString())) {
         this.name = name;
         this.desc = desc;
         this.project = project;
         this.dueDate = dueDate;
     };
 
-    createDate = dateFormatter(new Date()).formattedDate;
+    createDate  = new Date(new Date().toDateString());
+
+    // get dueDate() {
+    //     return this.displayDueDate();
+    // }
+
+    displayDueDate () {
+        let createDateTimeValue = this.createDate.getTime();
+        let dueDateTimeValue = this.dueDate.getTime();
+        if (createDateTimeValue === dueDateTimeValue) {
+            return "Today";
+        }
+        else {
+            return dateFormatter(this.dueDate).formattedDate;
+        }
+    }
+
+    // createDate = dateFormatter(new Date()).formattedDate;
 }
 
 function dateFormatter(date) {
@@ -23,7 +40,14 @@ function dateFormatter(date) {
     return { formattedDate }
 }
 
-newTask = new Task("Make bed","","",'Today');
+newTask = new Task("Make bed","","", new Date(2025, 2, 15));
 console.log(newTask.name);
-console.log(newTask.createDate)
+// console.log(newTask.createDate)
+console.log(newTask.displayDueDate());
+
+// dateOld = new Date(2025, 8, 15);
+// dateNew = new Date(2025, 8, 17);
+
+// console.log(dateOld)
+// console.log(dateNew - dateOld);
 
