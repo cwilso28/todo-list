dc85c04d4799a1b220ad9f1f7913a486ed4e9f23
Chris <47066922+cwilso28@users.noreply.github.com>
2025-03-25 21:23:07 -0500

    Add function to write tasks to DOM
    There needs to be a way to write the task list to the DOM. The added
    function writes multiple tasks to the DOM.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 class Task {
-    constructor(name, desc = '', project = 'Inbox', dueDate = new Date(new Date().toDateString())) {
+    constructor(name, 
+                desc = '', 
+                project = 'Inbox', 
+                dueDate = new Date(new Date().toDateString()), 
+                createDate = new Date(new Date().toDateString()), 
+                id = Date.now() + Math.random()){
         this.name = name;
         this.desc = desc;
         this.project = project;
         this.dueDate = dueDate;
+        this.createDate = createDate;
+        this.id = id;
     };
 
-    createDate  = new Date(new Date().toDateString());
-
+    createDate = new Date(new Date().toDateString());
     // get dueDate() {
     //     return this.displayDueDate();
     // }
@@ -40,10 +46,49 @@ function dateFormatter(date) {
     return { formattedDate }
 }
 
+function todoListWriter(array) {
+    let taskListContainer = document.getElementById("tasks");
+
+    for (const task of array) {
+        let taskContainer = document.createElement("div");
+        taskContainer.id = task.id;
+        taskContainer.dataset.createDate = task.createDate;
+
+        let taskName = document.createElement("p");
+        taskName.id = "name";
+        taskName.textContent = task.name;
+
+        let taskDesc = document.createElement("p");
+        taskDesc.id = "desc";
+        taskDesc.textContent = task.desc;
+
+        let taskProject = document.createElement("p");
+        taskProject.id = "project";
+        taskProject.textContent = task.project;
+
+        let taskDueDate = document.createElement("p");
+        taskDueDate.id = "due-date";
+        taskDueDate.textContent = task.displayDueDate();
+        
+        taskContainer.append(taskName);
+        taskContainer.append(taskDesc);
+        taskContainer.append(taskProject);
+        taskContainer.append(taskDueDate);
+
+        
+
+        taskListContainer.append(taskContainer);
+    }
+}
+
 newTask = new Task("Make bed","","", new Date(2025, 2, 15));
 console.log(newTask.name);
 // console.log(newTask.createDate)
 console.log(newTask.displayDueDate());
+
+taskArray = [newTask];
+
+todoListWriter(taskArray);
 
 // dateOld = new Date(2025, 8, 15);
 // dateNew = new Date(2025, 8, 17);
